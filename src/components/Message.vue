cb9368cd444e57d5293596fbb4543d809e63ba82
baiimbet0v <baiimbetov94@mail.ru>
2021-02-04 22:26:50 +0600

    second

---
+++ src/components/Message.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="message">
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>  
---
+++ src/components/Notes.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="notes">
+    <div class="note" :class="{full: !grid}" v-for="(note, index) in notes" :key="index">
+      <div class="note-header">
+        <p>{{ note.title }}</p>
+        <p style="cursor: pointer;" @click="removeNote(index)">x</p>
+      </div>
+      <div class="note-body">
+        <p>{{ note.descr }}</p>
+        <span>{{ note.date }}</span>
+      </div>
+    </div>
+  </div>
+</template>   
+
+<script>
+export default {
+  props: {
+    notes: {
+      type: Array,
+      required: true
+    },
+    grid: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    removeNote(index) {
+      console.log(`Note id - ${index} removed`);
+      this.$emit("remove", index);
+    }
+  }
+};
+</script> 
+
+<style lang="scss" >
+.notes {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 40px 0;
+}
+
+.note {
+  width: 48%;
+  padding: 18px 20px;
+  margin-bottom: 20px;
+  background-color: #fff;
+  &.full {
+    width: 100%;
+  }
+}
+
+.note-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  h1 {
+    font-size: 32px;
+  }
+  p {
+    color: #402caf;
+    font-size: 22px;
+  }
+  svg {
+    margin-right: 12px;
+    color: #999;
+    &.active {
+      color: #402caf;
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+}
+
+.note-body {
+  p {
+    margin: 20px 0;
+  }
+  span {
+    font-size: 14px;
+    color: #999;
+  }
+}
+</style>
